c0cdaa21d09baa783a05b348e0396e5b5bbbcd55
vivek1-satva <vivek1.satva@gmail.com>
2024-05-03 18:03:14 +0530

    created files in data access layer

--- DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08025fc2f6686ac096ffdec8ffb4ccd451659fac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a86afd6dd4ccb2b85df43971546b392108f1ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
